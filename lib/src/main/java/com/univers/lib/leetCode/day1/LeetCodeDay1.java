a94a40dc01976938e4f74e52b745d8ee9625ca96
JiaJie-xu1 <876102209@qq.com>
2020-12-02 22:55:48 +0800

    1.leetCode 算法复盘

--- lib/src/main/java/com/univers/lib/leetCode/day1/LeetCodeDay1.java
+++ lib/src/main/java/com/univers/lib/leetCode/day1/LeetCodeDay1.java
@@ -11,28 +11,43 @@ import java.util.Map;
  * @since 2020/11/30
  * 算法题1：两数之和
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
  * 示例:
  * 给定 nums = [2, 7, 11, 15], target = 9
- *
+ * <p>
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
  */
 public class LeetCodeDay1 {
 
     public static void main(String[] args) throws Exception {
-        System.out.println(Arrays.toString(twoSum(new int[]{2,9,7,11},13)));
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 9, 7, 11}, 13)));
     }
 
-    public static int[] twoSum(int[] nums, int target){
+    /**
+     *  返回符合条件的两数下标
+     */
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
+        //key存数字，value存下标，当map中存在了目标target - nums[i]的key,则找到满足条件的两数
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])){
-                return new int[]{map.get(target-nums[i]),i};
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("no data fit");
+    }
+
+    public static int[] twoNum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("no data");
     }
 }
