54be6e53318c743c6e21de9043b993caca8bc620
xujiajie <xujiajie@zhuge.com>
2021-11-24 17:26:54 +0800

    多数元素

---
+++ lib/src/main/java/com/univers/lib/leetCode/LeetCodeDay6.java
@@ -0,0 +1,54 @@
+package com.univers.lib.leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 多数元素
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[3,2,3]
+ * 输出：3
+ * 示例 2：
+ * <p>
+ * 输入：[2,2,1,1,1,2,2]
+ * 输出：2
+ *  
+ * <p>
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions/xm77tm/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class LeetCodeDay6 {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int temp = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                temp = entry.getKey();
+            }
+        }
+        return temp;
+    }
+}
